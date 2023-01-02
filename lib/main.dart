750fffff0bcef9a41b6cc5c7b62cb209ea1d7cca
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-01-02 23:55:46 +0900

    온보딩 스크린 추가 (내일은 디자인도 설정해야지~)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:routepractice/pages/create_account_page.dart';
-import 'package:routepractice/pages/intro_page.dart';
 import 'package:routepractice/pages/login_page.dart';
 import 'package:routepractice/pages/main_page.dart';
 import 'package:routepractice/pages/onboarding_page.dart';
@@ -11,50 +10,65 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        backgroundColor: Colors.white,
-        primarySwatch: Colors.blue,
-        primaryColorLight: Colors.white,
-        textTheme: TextTheme(
-            headline1 : TextStyle(fontFamily: 'NotoSansKR', fontWeight: FontWeight.bold, fontSize: 30, color: Colors.black ,
-                shadows: <Shadow>[
-                  Shadow(
-                    offset: Offset(3.0, 3.0), blurRadius: 10.0, color: Color.fromRGBO(255, 255, 255, 0.55)
-                  )
-                ] ),
-            bodyText1 : TextStyle(fontFamily: 'NotoSansKR', fontWeight: FontWeight.normal, fontSize: 10, color: Colors.black),
-            button: TextStyle(fontFamily: 'NotoSansKR', fontWeight: FontWeight.normal, fontSize: 12, color: Colors.white)
-        ),
-        appBarTheme: const AppBarTheme(
           backgroundColor: Colors.white,
-          shadowColor: Colors.white38,
-          iconTheme: IconThemeData(color: Colors.black54),
-          centerTitle: true,
-          elevation: 5,
-          titleTextStyle: TextStyle(color: Colors.black54, fontWeight: FontWeight.bold, fontSize: 13),
-        ),
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-            foregroundColor: Colors.white,
-            backgroundColor: Colors.blue,
-            elevation: 10,
-            shadowColor: Colors.white,
-            minimumSize: Size(100,15),
+          primarySwatch: Colors.blue,
+          primaryColorLight: Colors.white,
+          textTheme: const TextTheme(
+              headline1: TextStyle(
+                  fontFamily: 'NotoSansKR',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  color: Colors.black,
+                  shadows: <Shadow>[
+                    Shadow(
+                        offset: Offset(3.0, 3.0),
+                        blurRadius: 10.0,
+                        color: Color.fromRGBO(255, 255, 255, 0.55))
+                  ]),
+              bodyText1: TextStyle(
+                  fontFamily: 'NotoSansKR',
+                  fontWeight: FontWeight.normal,
+                  fontSize: 10,
+                  color: Colors.black),
+              button: TextStyle(
+                  fontFamily: 'NotoSansKR',
+                  fontWeight: FontWeight.normal,
+                  fontSize: 12,
+                  color: Colors.white)),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white,
+            shadowColor: Colors.white38,
+            iconTheme: IconThemeData(color: Colors.black54),
+            centerTitle: true,
+            elevation: 5,
+            titleTextStyle: TextStyle(
+                color: Colors.black54,
+                fontWeight: FontWeight.bold,
+                fontSize: 13),
           ),
-        )
-      ),
+          textButtonTheme: TextButtonThemeData(
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.blue,
+              elevation: 10,
+              shadowColor: Colors.white,
+              minimumSize: Size(100, 15),
+            ),
+          )),
       initialRoute: '/',
       routes: {
-        '/':(context)=>IntroPage(),
-        '/login':(context)=>LoginPage(),
-        '/createAccount' : (context)=>CreateAccountPage(),
-        '/main' : (context)=>MainPage(),
-        '/onboarding':(context)=>OnBoardingPage(),
+        '/': (context) => const OnBoardingPage(),
+        '/login': (context) => const LoginPage(),
+        '/createAccount': (context) => const CreateAccountPage(),
+        '/main': (context) => const MainPage(),
+        '/onboarding': (context) => const OnBoardingPage(),
       },
     );
   }
--- lib/pages/onboarding_page.dart
+++ lib/pages/onboarding_page.dart
@@ -6,18 +6,34 @@ class OnBoardingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Builder(builder: (BuildContext context) {
-      return IntroductionScreen(
-        pages: [
-          PageViewModel(
-              title: 'Welcome to my lovely app'
-          ),
-        ],
-        done: const Text('done'),
-        onDone: () {
-          Navigator.of(context).pop(OnBoardingPage());
-        },
-      );
-    });
+    return Scaffold(
+      body: SafeArea(
+        child: IntroductionScreen(
+          pages: [
+            PageViewModel(
+                title: '함께 성장',
+                body: '사랑하는 사람과 목표를 함께해요',
+                image: Image.asset('assets/images/onboarding/friendship.png'),
+                decoration: PageDecoration(),
+            ),
+            PageViewModel(
+                title: '즐거움',
+                body: '열정을 함께하고, 즐거움을 얻어요',
+                image: Image.asset('assets/images/onboarding/passion.png')),
+            PageViewModel(
+                title: '목표',
+                body: '사랑하는 사람과 꿈꾸던 미래를 만들어요',
+                image: Image.asset('assets/images/onboarding/marriage.png')),
+          ],
+          done: const Text('시작해요'),
+          onDone: () {
+            Navigator.of(context).pushNamed('/login');
+          },
+          next: const Icon(Icons.arrow_forward),
+        ),
+      ),
+    );
   }
+
+
 }
